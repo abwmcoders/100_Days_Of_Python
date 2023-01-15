6c48dbfd79ddfb5442434ac35de0823b11371dd0
abwmcode <abwmcode@gmail.com>
2023-01-15 09:34:58 +0100

    ref:day 6

--- Day_six/functions.py
+++ Day_six/functions.py
@@ -1,1 +1,34 @@
 reebok_robot = print('Reebok robot')
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# hurdle 4
+
+
+#def turn_right():##
+#    turn_left()
+#    turn_left()
+#    turn_left()
+
+
+#def jump():
+##   turn_left()
+#    while wall_on_right():
+ #       move()
+ #   turn_right()
+ #   move()
+ #   turn_right()
+ #   while front_is_clear():
+    #    move()
+ #   turn_left()
+
+
+#while not at_goal():
+ #   if wall_in_front():
+ #       jump()
+ #   else:
+ #       move()
+
+
+
+
